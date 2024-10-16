4cd8c66048e67eedceae78e07ff79d67fb8725f4
corcor322 <corcor322@gmail.com>
2024-10-16 11:05:13 -0400

    Updated todo

--- src/main/java/com/pluralsight/FinancialTracker.java
+++ src/main/java/com/pluralsight/FinancialTracker.java
@@ -1,5 +1,5 @@
 package com.pluralsight;
-//TODO: displayLedger, displayDeposits, displayPayments methods; Reports menu
+//TODO: add buffered writer to add user input to csv file; Reports menu
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
